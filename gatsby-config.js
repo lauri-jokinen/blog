cc59004e33957ad69a3d438bd6108c2da7bbb63d
Lauri Jokinen <65682962+lapamatoz@users.noreply.github.com>
2020-10-18 19:37:27 +0300

    title updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
+    title: 'Tiny Blog',
     author: 'Kyle Mathews',
     description: 'A starter blog demonstrating what Gatsby can do.',
     siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
