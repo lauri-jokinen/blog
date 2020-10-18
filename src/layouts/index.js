c9cb0ffed1e3f292d6e13392254f35683ed6e186
Lauri Jokinen <65682962+lapamatoz@users.noreply.github.com>
2020-10-18 15:43:47 +0300

    otsikko vaihdettu

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -30,7 +30,7 @@ class Template extends React.Component {
             }}
             to={'/'}
           >
-            Gatsby Starter Blog
+            Laurin blogi
           </Link>
         </h1>
       )
