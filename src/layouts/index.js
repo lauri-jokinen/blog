91a64934e4030e1a8be80fc0b96480351becaced
Lauri Jokinen <65682962+lapamatoz@users.noreply.github.com>
2020-10-18 19:38:10 +0300

    title updated

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -30,7 +30,7 @@ class Template extends React.Component {
             }}
             to={'/'}
           >
-            Tiny blog
+            Tiny Blog
           </Link>
         </h1>
       )
@@ -51,7 +51,7 @@ class Template extends React.Component {
             }}
             to={'/'}
           >
-            Tiny blog
+            Tiny Blog
           </Link>
         </h2>
       )
