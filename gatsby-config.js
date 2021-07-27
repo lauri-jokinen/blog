e757a27e7c4f70a8838daf22c6fdf562ec065204
Lauri Jokinen <65682962+lapamatoz@users.noreply.github.com>
2021-07-27 20:20:50 +0300

    Old version restored

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,33 @@
 module.exports = {
   siteMetadata: {
     title: 'Tiny Blog',
-    author: 'Lauri Jokinen',
-    description: 'A simple personal blog.',
-    siteUrl: 'https://blog.laurijokinen.com/',
+    author: 'Kyle Mathews',
+    description: 'A starter blog demonstrating what Gatsby can do.',
+    siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
   },
   pathPrefix: '/gatsby-starter-blog',
   plugins: [
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
       options: {
-        path: '${__dirname}/src/pages',
+        path: `${__dirname}/src/pages`,
         name: 'pages',
       },
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-images',
+            resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 590,
             },
           },
           {
-            resolve: 'gatsby-remark-responsive-iframe',
+            resolve: `gatsby-remark-responsive-iframe`,
             options: {
-              wrapperStyle: 'margin-bottom: 1.0725rem',
+              wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
           'gatsby-remark-prismjs',
@@ -36,17 +36,17 @@ module.exports = {
         ],
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
         //trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
-    'gatsby-plugin-feed',
-    'gatsby-plugin-offline',
-    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-feed`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: 'gatsby-plugin-typography',
       options: {
